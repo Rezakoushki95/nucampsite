3a033e9094a664186b924b8b53ecdef1a06dfc26
Rezakoushki95 <41579107+Rezakoushki95@users.noreply.github.com>
2023-09-22 17:40:24 +0200

    Add form validation to ContactForm component using Formik's validate prop and ErrorMessage component along with a custom validation function

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,6 @@
 import { Button, Label, Col, FormGroup } from "reactstrap";
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { validateContactForm } from "../utils/validateContactForm";
 
 const ContactForm = () => {
     const handleSumbit = (values, { resetForm }) => {
@@ -19,6 +20,7 @@ const ContactForm = () => {
                 feedback: ''
             }}
             onSubmit={handleSumbit}
+            validate={validateContactForm}
         >
             <Form>
                 <FormGroup row>
@@ -27,11 +29,14 @@ const ContactForm = () => {
                     </Label>
 
                     <Col md='10'>
-                        <Field 
+                        <Field
                             name='firstName'
                             placeholder='First Name'
                             className='form-control'
                         />
+                        <ErrorMessage name='firstName'>
+                            {(msg) => <p className='text-danger'>{msg}</p>}
+                        </ErrorMessage>
 
                     </Col>
 
@@ -43,11 +48,14 @@ const ContactForm = () => {
                     </Label>
 
                     <Col md='10'>
-                        <Field 
+                        <Field
                             name='lastName'
                             placeholder='Last Name'
                             className='form-control'
                         />
+                        <ErrorMessage name='lastName'>
+                            {(msg) => <p className='text-danger'>{msg}</p>}
+                        </ErrorMessage>
 
                     </Col>
 
@@ -59,11 +67,14 @@ const ContactForm = () => {
                     </Label>
 
                     <Col md='10'>
-                        <Field 
+                        <Field
                             name='phoneNum'
                             placeholder='Phone'
                             className='form-control'
                         />
+                        <ErrorMessage name='phoneNum'>
+                            {(msg) => <p className='text-danger'>{msg}</p>}
+                        </ErrorMessage>
 
                     </Col>
 
@@ -75,12 +86,15 @@ const ContactForm = () => {
                     </Label>
 
                     <Col md='10'>
-                        <Field 
-                            name ='email'
+                        <Field
+                            name='email'
                             placeholder='Email'
                             type='email'
                             className='form-control'
                         />
+                        <ErrorMessage name='email'>
+                            {(msg) => <p className='text-danger'>{msg}</p>}
+                        </ErrorMessage>
 
                     </Col>
 
@@ -88,16 +102,16 @@ const ContactForm = () => {
 
                 <FormGroup row>
                     <Label check md={{ size: 4, offset: 2 }}>
-                        <Field 
+                        <Field
                             name='agree'
                             type='checkbox'
-                            className='form-check-input'                          
+                            className='form-check-input'
                         />{' '}
-                            May we contact you?
+                        May we contact you?
                     </Label>
 
                     <Col md='4'>
-                        <Field 
+                        <Field
                             name='contactType'
                             as='select'
                             className='form-control'
@@ -111,11 +125,11 @@ const ContactForm = () => {
 
                 <FormGroup row>
                     <Label htmlFor="feedback" md='2'>
-                        Your Feedback 
+                        Your Feedback
                     </Label>
 
                     <Col md='10'>
-                        <Field 
+                        <Field
                             name='feedback'
                             as='textarea'
                             rows='12'
@@ -127,7 +141,7 @@ const ContactForm = () => {
 
                 <FormGroup row>
                     <Col md={{ size: 10, offset: 2 }}>
-                        <Button type='sumbit' color='primary'>
+                        <Button type='submit' color='primary'>
                             Send Feedback
                         </Button>
                     </Col>
